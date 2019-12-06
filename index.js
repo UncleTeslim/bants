6bc5c78a0f22147ed84601253c5d764719f04ccf
Your Name <teslimolukazeem@gmail.com>
2019-12-05 18:59:50 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+    res.send("Hey, welcome to my first express exercise!!!");
+});
+
+app.get("/epl/:club", function (req, res) {
+    var slangs = {
+        arsenal: "Good ebening",
+        chelsea: "The Pride Of London",
+        spurs: "Perennial Bottlers",
+        manu: "Ole At The Wheel",
+        liverpool: "Is This Our Year?"
+    }
+    var club = req.params.club;
+    var slang = slangs[club];
+    res.send(slang);
+});
+
+app.get("*", function (req, res) {
+    res.send("Sorry, only epl matters:} !!!");
+});
+
+app.listen(3000, function(){
+    console.log ("It worked!!");
+});
